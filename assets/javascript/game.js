f5892d58a4ed3b6cf47f210f7ef548ef93d0a052
danshook <shook.dan@gmail.com>
2018-08-08 12:34:48 -0700

    added image and basic css styling.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// declare array variable listOfWords
+// declare randomWord = generateRandomWord
+// declare lettersGuessed variable (array)
+// declare correctGuesses variable (array)
+
+// function generateRandomWord
+    // randomly pick a word from the array
+        // select random number from 0 - length of array
+        // return randomWord
+
+// Add event listener for user to press any key
+    // reset game
+        // update number of guesses to maximum
+        // clear variable lettersGuessed
+        // clear variable correctGuesses
+        // randomWord = generateRandomWord
+
+// choose a random word and display on screen
+
+// capture the users letter guessed
+    // compare letter guessed to our random word
