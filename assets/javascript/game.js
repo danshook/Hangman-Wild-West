b6d087d2237939ee33fe076e5e304ea88119c04f
danshook <shook.dan@gmail.com>
2018-08-09 19:51:07 -0700

    added syntax to pseudo code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,18 @@
 // declare array variable listOfWords
+var listOfWords =[];
 // declare randomWord = generateRandomWord
 // declare lettersGuessed variable (array)
+var lettersGuessed = [];
 // declare correctGuesses variable (array)
+var correctGuesses = [];
 
 // function generateRandomWord
     // randomly pick a word from the array
         // select random number from 0 - length of array
         // return randomWord
+        function generateRandomWord() {
+            return randomWord;
+        }
 
 // Add event listener for user to press any key
     // reset game
@@ -17,5 +23,6 @@
 
 // choose a random word and display on screen
 
-// capture the users letter guessed
-    // compare letter guessed to our random word
+// Add event listener to capture the users letter guessed
+    // store letter guessed in array
+    // compare letterGuessed to generateRandomWord
